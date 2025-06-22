598ee42ae0da7d75c524636ade1c71c8e818192c
Kanagavel V <38755689+KanagavelV@users.noreply.github.com>
2025-06-22 23:49:28 +0530

    Updates

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -17,8 +17,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23518, hash: '3ca03d208fbcc4cd135c54ef0395a9d5948dc4893f4fff5f49f6673adf4ee682', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17137, hash: '4c8367f724bc3c5de4004d7b3d1fb8dbbb4ce180f01d77ff5f9fdebf631d2ece', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23518, hash: 'c6bb37912877560545a92b93373845f94002769189b6d51ba4177f4d62b09a91', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17137, hash: '34150f291d6146b66e91114848d17082aa8d4059c198ab569ba026ead372fdec', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'home/index.html': {size: 61200, hash: '680618d4e2b1cca307c65fefab9fa4dfecd04b6b4102aff738cdbe42398cc283', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
     'styles-BVFWE4T2.css': {size: 6956, hash: 'f+pNaNrAc+8', text: () => import('./assets-chunks/styles-BVFWE4T2_css.mjs').then(m => m.default)}
   },
